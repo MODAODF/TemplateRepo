efa9f91979aa54be6191e39f7fc7dc31e694126f
nienzu <ibqqz0602@gmail.com>
2019-11-18 18:15:28 +0800

    Add extension icon & rewrite python conf script

--- pythonpath/TemplateRepoConf.py
+++ pythonpath/TemplateRepoConf.py
@@ -1,77 +1,91 @@
-import sys, os
+import sys
+import os
 import json
-InfoFilePath = r"templatesInfo.json"
-DiffFilePath = r"diffInfo.json"
-ServerSettingPath = r"server.json"
+InfoFile = r"templatesInfo.json"
+DiffFile = r"diffInfo.json"
+ServerSettingFile = r"server.json"
 syncCheckResultPath = r"sync_check.json"
-ProjectName  = "TemplateRepo.oxt"
+ProjectName = "TemplateRepo"
+
+
+def getProjectRootPath():
+    global ProjectName
+    outPath = ""
+    for path in sys.path:
+        if ProjectName in path and "oxt" in path:
+            outPath = path
+            break
+    pathToken = outPath.split('\\')
+    outPath = ""
+    for token in pathToken:
+        outPath += token
+        outPath += "\\"
+        if ProjectName in token:
+            break
+    return outPath
+
 
 def getServerAddress():
     try:
         serverSetting = {}
         with open(getServerSettingPath(), "r") as setting:
-           serverSetting = json.load(setting)
+            serverSetting = json.load(setting)
         apiBaseAddr = ""
         if serverSetting['httpMethod']:
             apiBaseAddr += "http://"
         else:
             apiBaseAddr += "https://"
-        apiBaseAddr += (serverSetting['ServerAddress'] + ":" + serverSetting['Port'] + "/")
+        apiBaseAddr += (serverSetting['ServerAddress'] +
+                        ":" + serverSetting['Port'] + "/")
         return apiBaseAddr
     except:
         return "http://127.0.0.1:22/"
-    
+
+
 def getAPIAddress_List():
     baseAddress = getServerAddress()
-    apiAddress  = baseAddress + 'lool/templaterepo/list'
-    return apiAddress 
+    apiAddress = baseAddress + 'lool/templaterepo/list'
+    return apiAddress
+
 
 def getAPIAddress_download():
     baseAddress = getServerAddress()
-    apiAddress  = baseAddress + 'lool/templaterepo/download'
+    apiAddress = baseAddress + 'lool/templaterepo/download'
     return apiAddress
+
 
 def getAPIAddress_Sync():
     baseAddress = getServerAddress()
-    apiAddress  = baseAddress + 'lool/templaterepo/sync'
+    apiAddress = baseAddress + 'lool/templaterepo/sync'
     return apiAddress
 
+
 def getProjectImagesPath():
-    global ProjectName
-    outPath = ""
-    for path in sys.path:
-        if ProjectName in path:
-            outPath = path
-            break
-    
-    outPath = outPath.split(ProjectName)[0] + ProjectName + "\\images\\"
+    outPath = getProjectRootPath() + "images\\"
     return outPath
 
+
 def getProjectDataPath():
-    global ProjectName
-    outPath = ""
-    for path in sys.path:
-        if ProjectName in path:
-            outPath = path
-            break
-    
-    outPath = outPath.split(ProjectName)[0] + ProjectName + "\\runTimeData\\"
+    outPath = getProjectRootPath() + "runTimeData\\"
     return outPath
+
 
 def getServerSettingPath():
     prDataPath = getProjectDataPath()
-    global InfoFilePath
-    return prDataPath + ServerSettingPath
+    return prDataPath + ServerSettingFile
+
 
 def getTemplateInfoPath():
     prDataPath = getProjectDataPath()
-    global InfoFilePath
-    return prDataPath + InfoFilePath
+    global InfoFile
+    return prDataPath + InfoFile
+
 
 def getDiffInfoPath():
     prDataPath = getProjectDataPath()
-    global DiffFilePath
-    return prDataPath + DiffFilePath
+    global DiffFile
+    return prDataPath + DiffFile
+
 
 def getUserTemplatePath():
     global ProjectName
@@ -80,11 +94,12 @@ def getUserTemplatePath():
         if ProjectName in path:
             outPath = path
             break
-    
-    outPath = outPath.split("uno_packages")[0] + "template\\" 
+
+    outPath = outPath.split("uno_packages")[0] + "template\\"
     if not os.path.exists(outPath):
         os.mkdir(outPath)
     return outPath
+
 
 def getSyncCheckResult():
     prDataPath = getProjectDataPath()
